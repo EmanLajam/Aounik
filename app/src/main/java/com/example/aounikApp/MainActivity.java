288deb36c321be3e494eecc02996cd382febf900
reem <reembojeer@gmail.com>
2020-02-25 19:28:29 +0300

    order_details

--- app/src/main/java/com/example/aounikApp/MainActivity.java
+++ app/src/main/java/com/example/aounikApp/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(getApplicationContext(),Select.class);
+                Intent intent = new Intent(getApplicationContext(),order_details.class);
                 startActivity(intent);
                 finish();
             }

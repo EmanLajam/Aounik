cf970a01219f5ee2340dec1557fdad683dc55888
reem <reembojeer@gmail.com>
2020-03-10 13:10:32 +0300

    filld

--- app/src/main/java/com/example/aounikApp/Regster.java
+++ app/src/main/java/com/example/aounikApp/Regster.java
@@ -6,7 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ShareCompat;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -61,23 +63,52 @@ public class Regster extends AppCompatActivity {
                 final User user = new User(edtUsername.getText().toString(),
                         edtPassword.getText().toString(),edtMail.getText().toString(),editID.getText().toString(),ediphon.getText().toString());
 
-                users.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        if(dataSnapshot.child(user.getID()).exists())
-                            Toast.makeText(Regster.this,"the user exist",Toast.LENGTH_SHORT).show();
-                        else {
-                            users.child(user.getID()).setValue(user);
-                            Toast.makeText(Regster.this,"successfully registered",Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                if (edtPassword.getText().toString().trim().isEmpty()|| ediphon.getText().toString().trim().isEmpty()|| edtUsername.getText().toString().trim().isEmpty()
+                        ||edtMail.getText().toString().trim().isEmpty()||editID.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(Regster.this,"please fill the empty fields .",Toast.LENGTH_SHORT).show();
+
+                
+
+               /* if (edtPassword.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(Regster.this,"Password is empty. Please fill it in .",Toast.LENGTH_SHORT).show();
+
+                }
+                if (edtUsername.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(Regster.this,"Username is empty. Please fill it in .",Toast.LENGTH_SHORT).show();
+
+                }
+                if (edtMail.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(Regster.this,"Email is empty. Please fill it in .",Toast.LENGTH_SHORT).show();
+
+                }
+                if (editID.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(Regster.this,"ID is empty. Please fill it in .",Toast.LENGTH_SHORT).show();
+
+                }
+                if (ediphon.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(Regster.this,"phone is empty. Please fill it in .",Toast.LENGTH_SHORT).show();
+*/
+                }
+
+                else {
+                    users.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.child(user.getID()).exists())
+                                Toast.makeText(Regster.this, "the user exist", Toast.LENGTH_SHORT).show();
+                            else {
+                                users.child(user.getID()).setValue(user);
+                                Toast.makeText(Regster.this, "successfully registered", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                            }
                         }
-                    }
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
 
-                    }
-                });
+                        }
+                    });
+                }
             }
         });
 

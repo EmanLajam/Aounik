7882b823cdfd2825276e45bfaf605c13a1c3b5b9
EmanLajam <eman.lajam7@gmail.com>
2020-04-09 16:47:15 +0300

    rating

--- app/src/main/java/com/example/aounikApp/Rating.java
+++ app/src/main/java/com/example/aounikApp/Rating.java
@@ -15,18 +15,14 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Rating extends AppCompatActivity {
 
 
     RatingBar ratingBar;
     Button btsubmit;
     private DatabaseReference mRatingBarCh;
-    int user = 0;
+    int user = 1;
     TextView review_count, review_average;
-    int count = 0;
 
 
     @Override
@@ -42,47 +38,37 @@ public class Rating extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                final DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
-                // final DatabaseReference mRatingBarCh = rootRef.child("ratings");
-
-                rootRef.child("Users").child("id").addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        float  Rating = ratingBar.getRating();
-                        float total = 0;
-                        float average = 0;
-                        String id = getIntent().getStringExtra("user_id");
-                        for (DataSnapshot child : dataSnapshot.getChildren()) {
-                            float rating = child.child("rating").getValue(float.class);
-                            int Count = child.child("count").getValue(int.class);
-                            Count = Count + 1 ;
-                            total = Rating + rating;
-                            average = total / count;
-
-                            if (child.getKey().equals(id)) {
-
-                                Map<String, Object> result = new HashMap<>();
-                                result.put("rating", average);
-                                result.put("count", Count);
-                                rootRef.child(id).updateChildren(result);
-                            }
-                            if (Count != 0) {
-                                ratingBar.setRating(total / count);
-                                review_count.setText(id);
-                                review_average.setText((total / Count) + "");
-                            }
-                        }}
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                        throw databaseError.toException(); // don't ignore errors
-                    }
-                });
-
+                DatabaseReference addref = FirebaseDatabase.getInstance().getReference().child("Ratings").child("reem").child("user" + user);
+                user++;
+                addref.setValue(ratingBar.getRating());
             }
-
-
         });
 
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        // final DatabaseReference mRatingBarCh = rootRef.child("ratings");
 
-}}
+        rootRef.child("Ratings").child("reem").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                int total = 0,
+                        count = 0;
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    int rating = dataSnapshot.child("user" + (count + 1)).getValue(Integer.class);
+                    total = total + rating;
+                    count = count + 1;
+                }
+                if (count != 0) {
+                    ratingBar.setRating(total / count);
+                    review_count.setText(count + "");
+                    review_average.setText(((float)total / (float)count) + "");
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                throw databaseError.toException(); // don't ignore errors
+            }
+        });
+
+    }
+}
